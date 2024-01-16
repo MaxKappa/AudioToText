57636ddb2ccbc3449369dc86a1a47e7520de872a
massimiliano.capparuccia@studenti.unimi.it <massimiliano.capparuccia@studenti.unimi.it>
2024-01-16 16:38:34 +0100

    aggiunta barra di caricamento

--- app/main.py
+++ app/main.py
@@ -1,29 +1,28 @@
 # main.py
-from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from flask import Flask, request, redirect, url_for, render_template, send_file, jsonify
 import os
 from werkzeug.utils import secure_filename
 from utils.audioFileManager import AudioFileManager
 from utils.audioTranscriber import AudioTranscriber
 import threading
 
-# Inizializzazione dell'app Flask e configurazione
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['TRANSCRIPTION_FOLDER'] = 'transcriptions/'
-app.config['ALLOWED_EXTENSIONS'] = {'mp3', 'wav', 'mp4'}
+app.config['ALLOWED_EXTENSIONS'] = {'mp3', 'wav', 'mp4', 'm4a'}
 audio_manager = AudioFileManager(app.config['UPLOAD_FOLDER'], app.config['TRANSCRIPTION_FOLDER'])
 transcriber = AudioTranscriber(audio_manager)
 
-# Funzione helper per controllare se il file è consentito
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-# Endpoint della pagina iniziale
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Endpoint per il caricamento dei file
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'audioFile' not in request.files:
@@ -31,27 +30,27 @@ def upload_file():
     file = request.files['audioFile']
     if file.filename == '' or not allowed_file(file.filename):
         return redirect(url_for('index'))
-    
-    # Salvataggio del file caricato
     filename = secure_filename(file.filename)
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(file_path)
-
-    # Avvio della trascrizione in un thread separato
     output_filename = filename.rsplit('.', 1)[0] + '.txt'
     transcription_thread = threading.Thread(target=transcribe_audio, args=(file_path, output_filename))
     transcription_thread.start()
 
     return render_template('loading.html', filename=output_filename)
 
-# Funzione per la trascrizione
 def transcribe_audio(file_path, output_filename):
     transcriber.transcribe(file_path, output_filename)
 
-# Endpoint per il download della trascrizione
 @app.route('/download/<filename>')
 def download_transcription(filename):
-    return send_from_directory(app.config['TRANSCRIPTION_FOLDER'], filename)
+    file_path = os.path.join(app.config['TRANSCRIPTION_FOLDER'], filename)
+    return send_file(file_path, as_attachment=True, download_name=filename)
 
+
+@app.route('/remaining-time')
+def remaining_time():
+    print(transcriber.getRemainingTime())
+    return jsonify({"remainingTime": str(transcriber.getRemainingTime()), "estimatedTotalTime": str(transcriber.getEstimatedTotalTime())})
 
 app.run(host='localhost', port=8181, debug=True)
--- app/utils/audioTranscriber.py
+++ app/utils/audioTranscriber.py
@@ -3,6 +3,7 @@ import logging
 from pydub import AudioSegment
 import time
 import os
+import threading
 
 class AudioTranscriber:
     def __init__(self, audio_file_manager, model_name='small'):
@@ -10,6 +11,9 @@ class AudioTranscriber:
         self.setup_logger()
         self.chunk_length_ms = 120000
         self.audio_file_manager = audio_file_manager
+        self.remaining_time = 0
+        self.time_lock = threading.Lock()
+        self.estimated_total_time = 0
 
     def setup_logger(self):
         self.logger = logging.getLogger(__name__)
@@ -26,16 +30,25 @@ class AudioTranscriber:
                 result = self.model.transcribe("temp_chunk.wav", fp16=False, language="Italian")
                 transcriptions.append(result['text'])
                 elapsed_time = time.time() - start_time
-                estimated_total_time = elapsed_time / (i + 1) * len(chunks)
-                remaining_time = estimated_total_time - elapsed_time
+                with self.time_lock:
+                    self.estimated_total_time = elapsed_time / (i + 1) * len(chunks)
+                with self.time_lock:
+                    self.remaining_time = self.estimated_total_time - elapsed_time
                 self.audio_file_manager.save_chunk(result["text"], output_file_name)
-                self.logger.info(f"Chunk {i+1}/{len(chunks)} trascritto. Tempo rimanente stimato: {remaining_time:.2f} secondi")
+                self.logger.info(f"Chunk {i+1}/{len(chunks)} trascritto. Tempo rimanente stimato: {self.remaining_time:.2f} secondi")
             os.remove("temp_chunk.wav")
             return ' '.join(transcriptions)
         except Exception as e:
             self.logger.error(f"Errore nella trascrizione del file {audio_path}: {e}")
             raise
+    
+    def getRemainingTime(self):
+        with self.time_lock:
+            return int(self.remaining_time)
 
+    def getEstimatedTotalTime(self):
+        with self.time_lock:
+            return int(self.estimated_total_time)
     def _split_into_chunks(self, audio):
         return [audio[i:i + self.chunk_length_ms] for i in range(0, len(audio), self.chunk_length_ms)]
 
